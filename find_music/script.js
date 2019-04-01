ba926a244b2d54dbb1c7cf5381ff26398960cdd4
ElenaWebDev <48467926+ElenaWebDev@users.noreply.github.com>
2019-04-01 19:25:39 +0300

    up

--- find_music/script.js
+++ find_music/script.js
@@ -17,9 +17,9 @@ let modal = document.getElementById('myModal');
 let btn = document.getElementById("add-btn");
 let span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+
+	$('#add-btn').on('touchend mouseup', function () {     modal.style.display = "block"; });
+
 span.onclick = function() {
     modal.style.display = "none";
 }
